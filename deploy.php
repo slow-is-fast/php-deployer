25548e484256b9522d94355061a8c215273a0eef
s4tar <s4tar@qq.com>
2017-12-21 15:53:19 +0800

    add deploy.php

---
+++ deploy.php
@@ -0,0 +1,37 @@
+<?php
+$_name = "S1dZ730R6pqTNjCg";
+$sapi = php_sapi_name();
+
+$flag_file = '/tmp/' . $_name;
+$post_cmd_file = '/tmp/' . $_name . "_post_cmd";
+# Client
+# add to cron task
+if ($sapi == 'cli') {
+    $minutes = 60;
+    while ($minutes--) {
+        var_dump($minutes);
+        if (!file_exists($flag_file)) {
+            sleep(1);
+            continue;
+        }
+        system('git pull');
+        @unlink($flag_file);
+
+        if (file_exists($post_cmd_file)) {
+            system('php artisan cache:clear');
+            system('php artisan view:clear');
+            system('php artisan config:clear');
+            system('composer dumpauto');
+            @unlink($post_cmd_file);
+        }
+        exit(0);
+    }
+}
+
+# Server
+$has_post_cmd = $_GET['post_cmd'];
+touch($flag_file);
+if ($has_post_cmd) {
+    touch($post_cmd_file);
+}
+exit(1);
